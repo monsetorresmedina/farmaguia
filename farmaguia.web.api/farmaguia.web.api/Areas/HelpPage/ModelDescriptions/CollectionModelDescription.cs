3ebb95090fc7f90d22de9797a25f107117be26e6
Cristian Giagante <cristiangiagante@hotmail.com>
2018-05-25 00:52:14 -0300

    Create web api project with asp.net web api

---
+++ farmaguia.web.api/farmaguia.web.api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace farmaguia.web.api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
